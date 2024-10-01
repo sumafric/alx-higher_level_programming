a009e2280be793e5a870004024922babee56e69f
Abel Sum <abelsum1330@gmail.com>
2024-10-01 16:56:49 -0700

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Class with a method
+'''
+
+
+class Square:
+    '''
+    square with a get area method
+    '''
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integerr")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
