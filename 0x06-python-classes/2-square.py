6eb9f4abd3b3991daea83f7f28d4e481e658ce1e
Abel Sum <abelsum1330@gmail.com>
2024-10-01 16:53:11 -0700

    Size Validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,14 +3,13 @@
 
 
 class Square:
-
+    '''
+    size and exeptions
+    '''
     def __init__(self, size=0):
-        """
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
