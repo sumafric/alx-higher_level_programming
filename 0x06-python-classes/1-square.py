760092b89195b4599eafa37419a3952bebb4b0c2
Abel Sum <abelsum1330@gmail.com>
2024-10-01 16:38:18 -0700

    Private attribute of the class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Defining the class'''
+
+
+class Square:
+    '''
+    private instance attribute of the class, size
+    '''
+
+    def __init__(self, size):
+        self.__size = size
