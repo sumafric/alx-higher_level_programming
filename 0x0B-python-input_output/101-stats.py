ad068b3fd690065a17b59fe5c2005459844159f5
Abel Sum <abelsum1330@gmail.com>
2024-10-15 16:04:14 -0700

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -36,12 +36,12 @@ try:
 
             if str(status) in status_codes.keys():
                 status_codes[str(status)] += 1
-        except ValueError:
+        except:
             pass
 
         try:
             file_size += int(pieces[-1])
-        except IndexError:
+        except:
             pass
 
         lc += 1
