9d9b6c347a4fb3d536d843b8506fa5aa2a17f691
Abel Sum <abelsum1330@gmail.com>
2024-10-16 19:01:36 -0700

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""MyList class"""
+
+
+class MyList(list):
+    """child class of list"""
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+
+    def print_sorted(self):
+        """Prints sorted list, ASC order"""
+        print(sorted(self))
