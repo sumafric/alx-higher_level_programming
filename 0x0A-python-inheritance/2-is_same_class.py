bf9f9628324404c5d0a1f5e2d72995b641ee0eaa
Abel Sum <abelsum1330@gmail.com>
2024-10-16 19:13:31 -0700

    Exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""Function is_same_clas"""
+"""Defines function is_same_class"""
 
 
 def is_same_class(obj, a_class):
-    return (type(obj) == a_class)
+    """If obj is === an instance of a_class - True.
+        Otherwise - False.
+    """
+    if type(obj) == a_class:
+        return True
+    return False
