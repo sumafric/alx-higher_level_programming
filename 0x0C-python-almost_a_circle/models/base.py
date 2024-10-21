9d010b9f65340eadfab27daaff1ae663c636e2ef
Abel Sum <abelsum1330@gmail.com>
2024-10-21 15:46:00 -0700

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines a base model class.
+"""
+
+
+class Base:
+    """
+    Represents the base model
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
