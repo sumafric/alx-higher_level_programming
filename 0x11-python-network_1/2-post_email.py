8ec2f7e5961d1d348a9531b31af34ffa8028d025
sumafric <abelsum1330@gmail.com>
2025-01-20 17:54:03 +0000

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""  script that takes in a URL and an email, 
+"""script that takes in a URL and an email, 
 sends a POST request to the passed URL with the
 email as a parameter, and displays the body of the response
 """
-import sys
-import urllib.parse
-import urllib.request
-
+from sys import argv
+from urllib import request
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    url = argv[1]
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    requests = request.Request(url)
+    with request.urlopen(requests) as response:
+        header = "X-Request-Id"
+        print(dict(response.headers).get(header))
